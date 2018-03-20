d7c29cdb71ac0f4b77bc7f645f3b591d970856ea
mrsekut <kouta.marusue@ksk-anl.com>
2018-03-20 18:29:44 +0900

    [Add]workser=4のときを追加

--- book/effectivePython/prac_multiproseccing.py
+++ book/effectivePython/prac_multiproseccing.py
@@ -23,6 +23,8 @@ numbers10   = [(1963309, 2265973), (2030677, 3814172), (1551645, 2229620), (2051
 #                (1963309, 2265973), (2030677, 3814172), (1551645, 2229620), (2051429, 2022086), (2796292, 2355256), (1972029, 3050755), (2790710, 3103175), (2897408, 3954239), (2807038, 9397858), (2302602, 2265934)]
 numbers = [numbers1,numbers2,numbers3,numbers4,numbers5,numbers6,numbers7,numbers8,numbers9,numbers10]
 
+
+
 ######################
 # 普通にやる
 ######################
@@ -51,6 +53,7 @@ for i in range(len(numbers)):
 # 10:took 2.059 seconds
 
 
+
 ######################
 # ThreadPoolExecutorを使う
 ######################
@@ -64,7 +67,7 @@ print('==========================')
 for i in range(len(numbers)):
     start = time.time()
     pool = ThreadPoolExecutor(max_workers=2)
-    results = list(pool.map(gcd, numbers[i]))
+    results = list(pool.map(gcd, numbers[i])) # I/O?
     end = time.time()
     the_list.append(end - start)
     print(f'{i+1}:took %.3f seconds' % (end - start))
@@ -82,14 +85,17 @@ for i in range(len(numbers)):
 # 10:took 2.011 seconds
 
 
+
 ######################
 # ProcessPoolExecutorを使う
 ######################
 from concurrent.futures import ProcessPoolExecutor
 
+# max_workers = 2のとき
+#=======================
 ppe_list = []
 print('==========================')
-print('ProcessPoolExecutor')
+print('ProcessPoolExecutor max_workers=2')
 print('==========================')
 
 for i in range(len(numbers)):
@@ -101,21 +107,56 @@ for i in range(len(numbers)):
     print(f'{i+1}:took %.3f seconds' % (end - start))
 
 # result
-# 1:took 0.172 seconds
-# 2:took 0.293 seconds
-# 3:took 0.400 seconds
-# 4:took 0.540 seconds
-# 5:took 0.521 seconds
-# 6:took 0.531 seconds
-# 7:took 0.754 seconds
-# 8:took 0.789 seconds
-# 9:took 0.989 seconds
-# 10:took 0.984 seconds
+# 1:took 0.204 seconds
+# 2:took 0.193 seconds
+# 3:took 0.327 seconds
+# 4:took 0.369 seconds
+# 5:took 0.530 seconds
+# 6:took 0.535 seconds
+# 7:took 0.775 seconds
+# 8:took 0.782 seconds
+# 9:took 1.012 seconds
+# 10:took 0.993 seconds
 
 
+
+# max_workers = 4のとき
+#=======================
+ppe_list_4 = []
+print('==========================')
+print('ProcessPoolExecutor max_workers=4')
+print('==========================')
+
+for i in range(len(numbers)):
+    start = time.time()
+    pool = ProcessPoolExecutor(max_workers=4)
+    results = list(pool.map(gcd, numbers[i]))
+    end = time.time()
+    ppe_list_4.append(end - start)
+    print(f'{i+1}:took %.3f seconds' % (end - start))
+
+# result
+# 1:took 0.210 seconds
+# 2:took 0.200 seconds
+# 3:took 0.276 seconds
+# 4:took 0.381 seconds
+# 5:took 0.580 seconds
+# 6:took 0.574 seconds
+# 7:took 0.752 seconds
+# 8:took 0.878 seconds
+# 9:took 1.058 seconds
+# 10:took 1.128 seconds
+
+
+
+
+######################
+# 可視化
+######################
 plt.plot(normal_result, marker='o', color="black", label="normal")
 plt.plot(the_list, marker='p', color="blue", label="ThreadPoolExecutor")
-plt.plot(ppe_list, marker='*', color="red", label="ProcessPoolExecutor")
+plt.plot(ppe_list, marker='*', color="red", label="ProcessPoolExecutor worker=2")
+plt.plot(ppe_list_4, marker='8', color="green", label="ProcessPoolExecutor worker=4")
 plt.legend(loc="upper left")
 plt.xlabel('element_num')
 plt.ylabel('time')
